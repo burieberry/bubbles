ce44d4397d1d9ff84395dc19ba4109e0330fca2e
Burcu Noyan <burcunoyan@gmail.com>
2017-12-01 01:13:28 -0500

    init

---
+++ bubble.js
@@ -0,0 +1,227 @@
+function bubbleChart() {
+    var width = 960,
+        height = 960,
+        columnForColors = "status",
+        columnForRadius = "priority";
+
+    function chart(selection) {
+        var data = selection.enter().data();
+        var div = selection,
+            svg = div.selectAll('svg');
+        svg.attr('width', width).attr('height', height);
+
+        var tooltip = selection
+            .append("div")
+            .style("position", "absolute")
+            .style("visibility", "hidden")
+            .style("color", "white")
+            .style("padding", "8px")
+            .style("background-color", "#626D71")
+            .style("border-radius", "6px")
+            .style("text-align", "center")
+            .style("font-family", "monospace")
+            .style("width", "400px")
+            .text("");
+
+
+        var simulation = d3.forceSimulation(data)
+            .force("charge", d3.forceManyBody().strength([-600]))
+            .force("x", d3.forceX())
+            .force("y", d3.forceY())
+            .on("tick", ticked);
+
+        function ticked(e) {
+            node.attr("cx", function(d) {
+                    return d.x + 10;
+                })
+                .attr("cy", function(d) {
+                    return d.y;
+                });
+        }
+
+        var colorCircles = d3.scaleOrdinal(d3.schemePastel2);
+        var scaleRadius = d3.scaleLinear().domain([d3.min(data, function(d) {
+            return +d[columnForRadius];
+        }), d3.max(data, function(d) {
+            return +d[columnForRadius];
+        })]).range([10, 100])
+
+        var node = svg.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr('r', function(d) {
+                return scaleRadius(d[columnForRadius])
+            })
+            .style("fill", function(d) {
+                return colorCircles(d[columnForColors])
+            })
+            .attr('transform', 'translate(' + [width / 2, height / 2] + ')')
+            .on("mouseover", function(d) {
+                tooltip.html(d.title + "<br>" + "priority: " + d[columnForRadius] + "<br> status: " + d[columnForColors]);
+                return tooltip.style("visibility", "visible");
+            })
+            .on("mousemove", function() {
+                return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+            })
+            .on("mouseout", function() {
+                return tooltip.style("visibility", "hidden");
+            })
+            .call(d3.drag()
+              .on("start", dragstarted)
+              .on("drag", dragged)
+              .on("end", dragended))
+
+        function dragstarted(d) {
+          if (!d3.event.active) simulation.alphaTarget(1).restart();
+          d.fx = d.x;
+          d.fy = d.y;
+          return tooltip.style("visibility", "hidden");
+        }
+
+        function dragged(d) {
+          d.fx = d3.event.x;
+          d.fy = d3.event.y;
+          return tooltip.style("visibility", "hidden");
+        }
+
+        function dragended(d) {
+          if (!d3.event.active) simulation.alphaTarget(0);
+          d.fx = null;
+          d.fy = null;
+        }
+    }
+
+    chart.width = function(value) {
+        if (!arguments.length) {
+            return width;
+        }
+        width = value;
+        return chart;
+    };
+
+    chart.height = function(value) {
+        if (!arguments.length) {
+            return height;
+        }
+        height = value;
+        return chart;
+    };
+
+
+    chart.columnForColors = function(value) {
+        if (!arguments.columnForColors) {
+            return columnForColors;
+        }
+        columnForColors = value;
+        return chart;
+    };
+
+    chart.columnForRadius = function(value) {
+        if (!arguments.columnForRadius) {
+            return columnForRadius;
+        }
+        columnForRadius = value;
+        return chart;
+    };
+
+    return chart;
+}
+
+
+// var bubbleChart = function() {
+//   var width = 960,
+//       height = 960,
+//       maxRadius = 6,
+//       columnForColors = "category",
+//       columnForRadius = "views";
+
+//   function chart(selection) {
+//     var data = selection.enter().data();
+//     var div = selection,
+//         svg = div.selectAll('svg');
+//     svg.attr('width', width).attr('height', height);
+
+//     var tooltip = selection
+//       .append("div")
+//       .style("position", "absolute")
+//       .style("visibility", "hidden")
+//       .style("color", "white")
+//       .style("padding", "8px")
+//       .style("background-color", "#626D71")
+//       .style("border-radius", "6px")
+//       .style("text-align", "center")
+//       .style("font-family", "monospace")
+//       .style("width", "400px")
+//       .text("");
+
+//     var simulation = d3.forceSimulation(data)
+//       .force('charge', d3.forceManyBody().strength([-50]))
+//       .force('x', d3.forceX(0))
+//       .force('y', d3.forceY(0))
+//       .on('tick', ticked);
+
+//     function ticked(e) {
+//       node.attr('cx', function(d) { return d.x });
+//       node.attr('cy', function(d) { return d.y });
+//     }
+
+//     var colorCircles = d3.scaleOrdinal(d3.schemeCategory10);
+//     var scaleRadius = d3.scaleLinear()
+//       .domain([ d3.min(data, function(d) { return +d[columnForRadius] }),
+//                 d3.max(data, function(d) { return +d[columnForRadius] }) ])
+//       .range([5,18]);
+
+//     var node = svg.selectAll('circle')
+//       .data(data)
+//       .enter()
+//       .append('circle')
+//       .attr('r', function(d) { return console.log(d) })
+//       .style('fill', function(d) { return colorCircles(d[columnForColors]) })
+//       .attr('transform', 'translate(' + [width / 2, height / 2] + ')')
+//       .on('mouseover', function(d) {
+//         tooltip.html(d[columnForColors] + d.title + d[columnForRadius]);
+//         return tooltip.style('visibility', 'visible')
+//       })
+//       .on('movemove', function(d) {
+//         return tooltip
+//           .style('top', (d3.event.pageY - 10) + 'px' )
+//           .style('left', (d3.event.pageX + 10) + 'px');
+//       })
+//       .on('mouseout', function(d) {
+//         return tooltip.style('visibility', 'hidden');
+//       });
+//   }
+
+//   chart.width = function(val) {
+//     if (!arguments.length) return width;
+//     width = val;
+//     return chart;
+//   };
+
+//   chart.height = function(val) {
+//     if (!arguments.length) return height;
+//     height = val;
+//     return chart;
+//   };
+
+//   chart.columnForColors = function(value) {
+//         if (!arguments.columnForColors) {
+//             return columnForColors;
+//         }
+//         columnForColors = value;
+//         return chart;
+//     };
+
+//     chart.columnForRadius = function(value) {
+//         if (!arguments.columnForRadius) {
+//             return columnForRadius;
+//         }
+//         columnForRadius = value;
+//         return chart;
+//     };
+
+//   return chart;
+// };
+
+
